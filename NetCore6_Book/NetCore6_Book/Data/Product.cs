4e07866c5466981ec816457b82cad55396576568
DatNguyen1091 <nvdat0204@gmail.com>
2023-03-08 13:07:07 +0700

    WebApi:Data create  class Product, class ProductStoreContext; ; builder AddCors,AddDbcontext

---
+++ NetCore6_Book/NetCore6_Book/Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCore6_Book.Data
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]   
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+        [Range(0, 500)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ NetCore6_Book/NetCore6_Book/Data/ProductStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetCore6_Book.Data
+{
+    public class ProductStoreContext : DbContext // quan ly class tuong ung voi database
+    {
+        public ProductStoreContext(DbContextOptions<ProductStoreContext> options) : base(options)
+        {
+
+        }
+
+        #region DbSet
+        public DbSet<Product>? Products { get; set;}
+        #endregion
+    }
+}
